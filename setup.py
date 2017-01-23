3d457168606bb8125d16e65431f89266c8b92123
asystemdwq <michaelw1552@outlook.com>
2017-01-23 21:07:11 +0800

    add a setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 08:06:12) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from distutils.core import setup
+>>> setup (
+	name       = 'print',
+	version    = '1.0.0',
+	py_modules = ['print'],
+	author     = 'Micahel',
+	author_email = 'Michaelw1552@outlook.com',
+	url 	   = 'http://www.baidu.com',
+	description = 'A simple print of list',
+	)
