06720162a65d11353fa914253b6c8dea9c46f7c6
asystemdwq <michaelw1552@outlook.com>
2017-02-14 09:34:51 +0800

    Add Some Powershell Scripts

---
+++ Change meetingrooms Num.ps1
@@ -0,0 +1,9 @@
+﻿#This is a script created by Michael Wang.
+$Roominfo = Import-Csv "C:\Users\michael_wang\Documents\meetingrooms.csv"
+foreach ($room in $Roominfo)
+{
+    $Num = $room.Num
+    $Names = $room.Names
+   $Sam = Get-ADUser -Filter {name -like $Names} | select $_.SamAccountName
+    Set-ADUser $Sam -OfficePhone $Num
+}
---
+++ Connect_Exchange.ps1
@@ -0,0 +1,5 @@
+﻿$username = "resourcepro0\mwang"
+$passwrod = ConvertTo-SecureString "Init_8852"
+$cred = new-object -typename System.Management.Automation.PSCredential -ArgumentList $username, $passwrod
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://us-exch-hybrid2.resourcepro0.resourcepro.com/powershell -Credential $cred -Authentication default
+Import-PSSession $Session
