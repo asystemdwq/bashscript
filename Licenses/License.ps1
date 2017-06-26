ddd3c46b42e133d21b8564e542cd26990325cdb8
root <root@localhost.localdomain>
2017-06-26 15:35:25 +0800

    Add Office 365 Licenses batchfiles

---
+++ Licenses/License.ps1
@@ -0,0 +1,17 @@
+﻿$userinfo = Import-Csv "C:\licenses\userlist.csv"
+$username = "Michael_Wang@resourcepro.com.cn"
+$password = cat C:\Licenses\pass.txt | ConvertTo-SecureString
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+Connect-MsolService -Credential $cred
+foreach ($users in $userinfo)
+{
+    $user = $users.Name
+    $Add = $users.Add
+    $Remove = "resourcepro0" + ":" + $users.Remove
+    if ($user)
+    {
+        Set-MsolUserLicense -UserPrincipalName $user -RemoveLicenses "resourcepro:$($Remove)"
+        if ($Add)
+        {set-msoluserLicense -UserPrincipalName $user -AddLicenses "resourcepro:$($Add)"}
+    }
+}
