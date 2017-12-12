0a6966d8e0d7286ae9622dd4caec24ddec2fe5df
root <root@localhost.localdomain>
2017-12-12 11:36:17 +0800

    Update Migrate 365 Scirpts

--- Migrate365/migrate.ps1
+++ Migrate365/migrate.ps1
@@ -1,16 +1,37 @@
 ﻿#This is script is created by Michael Wang. The perpose is migrate users to office 365
-$userinfo = Import-csv "C:\Migrate365\userinfo.csv"
-#Import Exhcange mode
-$username = "resourcepro0\mwang"
-$passwrod = "Init_8852"
-$cred = new-object -typename System.Management.Automation.PSCredential -ArgumentList $username, $passwrod
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://us-exch-hybrid2.resourcepro0.resourcepro.com/powershell -Credential $cred -Authentication default
-Import-PSSession $Session
+$userinfo = Import-csv "C:\scripts\Migrate365\userinfo.csv"
 #Check user status & migrate it to office
 foreach ($user in $userinfo)
 {
+#Import Exhcange mode
+$username = "resourcepro0\mwang"
+$365username = "michael_wang@resourcepro.com.cn"
+$password = Get-Content C:\scripts\Migrate365\pass.txt | ConvertTo-SecureString
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+$365cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $365username, $password
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://us-exch-hybrid2.resourcepro0.resourcepro.com/powershell -Credential $cred -Authentication default
+Import-PSSession $Session
 $name1 = $user.name1
-if ($name1) {if (get-mailbox $name1) {new-moverequest -identity $name1 -Baditemlimit "200"} else {echo "$name1 is already on office365"}}
+if ($name1) {if (get-mailbox $name1) {new-moverequest -identity $name1 -Baditemlimit "200"} else {echo "$name1 is already on office365"
+continue}}
 #Check Move Request status
-if ($name1) {get-moverequest $name1}
+Do
+{
+    $status = (Get-MoveRequest | select Alias, status |Where-Object {$_.Alias -eq "$name1"}).status
+    Switch ($status)
+
+    {
+        "Inprogress" {
+            Write-Output "Migrate $name1 Still in progress"
+        }
+        "Completed"
+        {
+            Write-Output "Migrate $name1 job is completed, strat to migrate $name1 mailbox to o365"
+            $365Session = New-pssession –configurationname Microsoft.exchange –connectionuri https://ps.outlook.com/powershell/ -credential $365cred –authentication basic –allowredirection
+            Import-PSSession $365Session
+        }
+    }
+} Until ($status -eq "Completed")
+
+
 }
---
+++ Migrate365/to365.ps1
@@ -0,0 +1,16 @@
+﻿#This is script is created by Michael Wang. The perpose is migrate users to office 365
+$userinfo = Import-csv "C:\scripts\Migrate365\userinfo.csv"
+#Import Exhcange mode
+$username = "resourcepro0\mwang"
+$365username = "michael_wang@resourcepro.com.cn"
+$password = Get-Content C:\scripts\Migrate365\pass.txt | ConvertTo-SecureString
+$365cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($365username, $password)
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+$Session = New-pssession –configurationname Microsoft.exchange –connectionuri https://ps.outlook.com/powershell/ -credential $365cred –authentication basic –allowredirection
+Import-PSSession $Session
+#Check user status & migrate it to office
+foreach ($user in $userinfo)
+{
+$name1 = $user.name1
+New-moverequest –identity $name1 –remote –remotehostname rsp-us-mx1.resourcepro.com.cn –remotecredential $cred –targetdeliverydomain resourcepro.mail.onmicrosoft.com -BadItemLimit 200
+}
